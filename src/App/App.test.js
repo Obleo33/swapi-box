105c794c8a5b959a395a0d5aa53c3b3008d76cf4
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-21 13:16:44 -0600

    3 tests written for app.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('should have an intial state of null for its props', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.state().data).toEqual({});
+  });
+
+  // it('should have a component called ScrollingText', () => {
+  //   const wrapper = shallow(<App />)
+  //
+  //   expect(wrapper.find(ScrollingText)).to.have.length.of(1);
+  // });
 });
