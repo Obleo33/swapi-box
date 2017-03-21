0527b9efa1a8dbff614baec28a44e8dd71b2aaf5
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-21 15:12:25 -0600

    Add rendering of movie script.

--- src/Film/Film.js
+++ src/Film/Film.js
@@ -4,9 +4,14 @@ import React from 'react';
 
 const Film = ( { filmData } ) => {
   if(filmData){
+    console.log(filmData)
     const random = Math.floor((Math.random() * filmData.length) )
     return(
-      <div>{filmData[random].opening_crawl}</div>
+      <div>
+        <p>{filmData[random].opening_crawl}</p>
+        <p>{filmData[random].title}</p>
+        <p>{filmData[random].release_date}</p>
+      </div>
     )
   } else {
     return(
