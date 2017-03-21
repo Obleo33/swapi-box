05ff359dcbb38f06e785af27772c6536f0c1333c
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-21 13:16:16 -0600

    Create  new folder for film

---
+++ src/Film/Film.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const ScrollingText = ({ filmData }) => {
+
+const filmText = Object.keys(filmData).map((key, i) => {
+  console.log(key[results])
+})
+
+  return(
+    <div>test</div>
+  )
+}
+
+export default ScrollingText;
