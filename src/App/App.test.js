4decb0ac6a76a954662e0bc9231f541f28d4db55
Franklin Crosby <obleo33@gmail.com>
2017-03-21 13:11:51 -0600

    Add folders for components

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('has a dive with the class of swapi', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper.find('.app'))
+  })
+
+
+})
